daa60f4031a436645cb8f9d0e93bb9b01a950924
kagudkovArt <konstantin.gudkov@picsart.com>
2019-09-23 19:39:15 +0300

    refresh classes

--- sort_images.py
+++ sort_images.py
@@ -5,9 +5,9 @@ import shutil
 import cv2
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--source_folder', dest='source_folder',
+parser.add_argument('--source_folder', dest='source_folder', default='sources',
                     help='path to unsorted images')
-parser.add_argument('--res_folder', dest='res_folder',
+parser.add_argument('--res_folder', dest='res_folder', default='results',
                     help='path result classes folders')
 args = parser.parse_args()
 
@@ -15,19 +15,16 @@ key_map = {
     ord('1'): "OK",
     ord('2'): "BigAngle",
     ord('3'): "BigInpaintingArea",
-    ord('4'): "BlueBlobs",
-    ord('5'): "HairsOnEyes",
-    ord('6'): "HalfEyeBlending",
-    ord('7'): "HandsOnHead",
-    ord('8'): "Hat",
-    ord('9'): "HolesInHead",
-    ord('0'): "Lashes",
-    ord('q'): "OtherProblems",
-    ord('w'): "ThingsBeforeFace",
-    ord('e'): "WrongAngle",
-    ord('t'): "TwoAndMorePeople",
-    ord(' '): "AlreadyOperated",
+    ord('4'): "HairsOnEyes",
+    ord('5'): "HandsOnHead",
+    ord('6'): "Hat",
+    ord('7'): "ObjectInFrontOfFace",
+    ord('8'): "OtherProblems",
+    ord('9'): "TwoAndMorePeople",
+    ord('0'): "WrongAngle",
+    ord(' '): "NotOK",
 }
+
 for folder_name in key_map.values():
     os.makedirs(os.path.join(args.res_folder, folder_name), exist_ok=True)
 
@@ -41,8 +38,7 @@ for image_name in os.listdir(args.source_folder):
             continue
         folder_name = key_map[code]
         shutil.copy2(image_path, os.path.join(args.res_folder, folder_name, image_name))
-        if folder_name == 'AlreadyOperated' or folder_name == 'OK':
-            shutil.copy2(image_path, os.path.join(args.res_folder, 'AlreadyOperated', image_name))
+        if folder_name == 'NotOK' or folder_name == 'OK':
             os.remove(image_path)
             cv2.destroyAllWindows()
             break
